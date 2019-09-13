38d61e74cbfc5f4592f05ee81c60b0eb0f7b7ab4
mayjue <juem@berea.edu>
2019-09-12 21:55:16 -0400

    Added pictures and styling

---
+++ index.js
@@ -0,0 +1,14 @@
+function change(){
+  var x = document.getElementById("drawing2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  var y = document.getElementById("drawing");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
